f0fad0c0c10e22b4727ed0924eb526c756380467
Deku-foryou <reyhanmagribi57@gmail.com>
2026-01-16 19:33:39 +0700

    completing add information

--- app/(landing)/components/checkout/order-information.tsx
+++ app/(landing)/components/checkout/order-information.tsx
@@ -4,6 +4,20 @@ const OrderInformation = () => {
          <div className="p-4 border-b border-gray-200">
             <h2 className="font-bold text-lg">Order Information</h2>
          </div>
+         <div className="p-5">
+            <div className="input-group">
+               <label htmlFor="full_name">Full Name</label>
+               <input type="text" placeholder="Type Your Full Name" id="full_name" />
+            </div>
+             <div className="input-group">
+               <label htmlFor="wa_number">Whatsapp Number</label>
+               <input type="text" placeholder="+62xxx" id="wa_number" />
+            </div>
+             <div className="input-group">
+               <label htmlFor="shipping_address">Shipping Address</label>
+               <textarea placeholder="Example Street, 18, West Jakarta, Indonesia, 66521" id="shipping_address" rows={7} />
+            </div>
+         </div>
       </div>
    );
 };
