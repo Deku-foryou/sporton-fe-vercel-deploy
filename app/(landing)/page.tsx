8f60c71ca5117be00318a40af50c594ca84f828a
Deku-foryou <reyhanmagribi57@gmail.com>
2026-01-27 20:56:41 +0700

    fixing error img url

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,12 +1,12 @@
 import HeroSection from "./components/home/hero";
 import CategoriesSection from "./components/home/categories";
 import ProductsSection from "./components/home/products";
-import { getAllcategories } from "../services/category-service";
+import { getAllCategories } from "../services/category-service";
 import { getAllProducts } from "../services/product-service";
 
 export default async function Home() {
   const [categories, products] = await Promise.all([
-    getAllcategories(),
+    getAllCategories(),
     getAllProducts()
   ])
   return (
